b0dfc0e3f9a42a4ceb5612b21f3db52349714d3a
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-09-30 20:42:36 +0300

    first commit

---
+++ app/src/main/java/com/example/rufoody/MainMenu.java
@@ -0,0 +1,65 @@
+package com.example.rufoody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    Button signinemail,signinphone,signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        final Animation zoomin = AnimationUtils.loadAnimation(this,R.anim.zoomin);
+        final Animation zoomout = AnimationUtils.loadAnimation(this,R.anim.zoomout);
+
+        signinemail=(Button)findViewById(R.id.SignwithEmail);
+        signinphone=(Button)findViewById(R.id.SignwithPhone);
+        signup=(Button)findViewById(R.id.Signup);
+
+        signinemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signphone = new Intent(MainMenu.this,ChooseOne.class);
+                signphone.putExtra("Home","Phone");
+                startActivity(signphone);
+                finish();
+            }
+        });
+
+        signinphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signphone = new Intent(MainMenu.this,ChooseOne.class);
+                signphone.putExtra("Home","Phone");
+                startActivity(signphone);
+                finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainMenu.this,ChooseOne.class);
+                signup.putExtra("Home","SignUp");
+                startActivity(signup);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
+
+    }
+}
